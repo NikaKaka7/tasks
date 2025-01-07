8ff2c123068329bfc52b93cc2bebba9bad5438cf
NikaKaka7 <nikakakashvili077@gmail.com>
2025-01-07 23:37:39 +0400

    Add files via upload
    math interpreter task

---
+++ interpreter/interpreter.py
@@ -0,0 +1,12 @@
+expression = input("Expression: ")
+x, y, z = expression.split(" ")
+x = int(x)
+z = int(z)
+if y == "/":
+    print(float(x/z))
+elif y == "+":
+    print(float(x+z))
+elif y == "-":
+    print(float(x-z))
+elif y == "*":
+    print(float(x*z))
